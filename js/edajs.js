6de18df79883a35e0ea1b9fdd46963ef94fe0436
rvillegas505 <rvillegas@alumno.unlam.edu.ar>
2020-11-04 23:34:24 -0300

    casi terminado

--- js/edajs.js
+++ js/edajs.js
@@ -1,15 +1,16 @@
-function contarcarac()
+function contarcarac() 
 {
     var maxLength = 1000;
     var strLength = document.getElementById("consultaform").value.length;
     var charRemain = (maxLength - strLength);
     
-    if(charRemain < 0){
+    if(charRemain < 0) {
         document.getElementById("caracteresRestantes").innerHTML = '<span style="color: red;">Usted ha excedido el limite de '+maxLength+' caracteres</span>';
     }else{
         document.getElementById("caracteresRestantes").innerHTML = charRemain+' caracteres restantes';
     }
 }
+
 
 function valida_envia()
 {
@@ -51,7 +52,7 @@ function valida_envia()
 		var c_email=true;
 	}
 
-	//Validamos un campo o área de texto, por ejemplo el campo nombre
+	//Validamos un campo o área de texto, por ejemplo el campo consulta
 	if (document.form.consultaform.value.length==0)
 	{
 		alert("Tiene que escribir una consulta")
@@ -63,8 +64,40 @@ function valida_envia()
 	document.form.submit();
 }
 
+function mostrarPaises()
+{
+	if (form.sinoViaje[0].checked == true){
+		document.getElementById('buscadorPais').style.display = 'block';	//si la opcion elegida es Si, muestra el listado de paises
+		
+	}
+	else{
+		document.getElementById('buscadorPais').style.display = 'none';		//si la opcion elegida es No, mantiene oculto el listado de paises 
+	}
+
+	if (form.sinoViaje[0].checked == true){
+		document.getElementById('buscadorPaisT').style.display = 'block';	//si la opcion elegida es Si, muestra el listado de paises
+		
+	}
+	else{
+		document.getElementById('buscadorPaisT').style.display = 'none';		//si la opcion elegida es No, mantiene oculto el listado de paises 
+	}
+}
+
+function mostrarDireccion()
+{
+	if (form.sinoRespirar[0].checked == true){
+		document.getElementById('direccion').style.display = 'block';	//si la opcion elegida es Si, muestra el campo Direccion
+	}
+	else{
+		document.getElementById('direccion').style.display = 'none';	//si la opcion elegida es No, mantiene oculto el campo Direccion
+	}
+}
+
+
 function valida_enviacorona()
 {
+	var sintomas = 0;	//declaramos variable para almacenar los sintomas que sean marcados en el form
+
 	//Definimos los caracteres permitidos en una dirección de correo electrónico
 	var regexp = /^[0-9a-zA-Z._.-]+\@[0-9a-zA-Z._.-]+\.[0-9a-zA-Z]+$/;
 
@@ -76,6 +109,16 @@ function valida_enviacorona()
 		return 0;
 	}
 
+    
+	//Validamos un campo dni
+	valor = document.form.dni.value;
+	if( !(/^\d{8}$/.test(valor)) ) 
+	{
+		alert("Debe escribir un DNI");
+		document.form.dni.focus();
+		return 0;
+	}
+    
 
 	//Validamos un campo de texto como numérico, por ejemplo el campo teléfono de 9 dígitos
 	valor = document.form.telefono.value;
@@ -86,48 +129,69 @@ function valida_enviacorona()
 		return 0;
 	}
 
-	//Validamos un campo dni
-	valor = document.form.dni.value;
-	if( !(/^\d{8}$/.test(valor)) ) 
-	{
-		alert("Debe escribir un DNI");
-		document.form.dni.focus();
-		return 0;
-	}
 
 	//validar radio
-	//var validarRadio=function(){
-	if (form.sinoFiebre[0].checked == true || form.sinoFiebre[1].checked == true ){
-	}else{
-		alert("Completa el campo fiebre");
+	//var validarRadio=function()
+	
+
+	if (form.sinoFiebre[0].checked == true) {
+		sintomas++;									//en la variable Sintomas almacenamos aquellos que fueron seleccionados
+	}
+	else if (form.sinoFiebre[1].checked == true) {
+	}
+	else{
+		alert("Responde si has tenido fiebre");
 		return false;
 	}
 
-	if (form.sinoCabeza[0].checked == true || form.sinoCabeza[1].checked == true ){
-	}else{
+	
+	if (form.sinoCabeza[0].checked == true) {
+		sintomas++;
+	}
+	else if (form.sinoCabeza[1].checked == true) {
+	}
+	else{
 		alert("Responde si has tenido dolor de cabeza");
 		return false;
 	}
-
-	if (form.sinoTos[0].checked == true || form.sinoTos[1].checked == true ){
-	}else{
+	
+	
+	if (form.sinoTos[0].checked == true) {
+		sintomas++;
+	}
+	else if (form.sinoTos[1].checked == true) {
+	}
+	else{
 		alert("Responde si has tenido tos");
 		return false;
 	}
-
-	if (form.sinoGarganta[0].checked == true || form.sinoGarganta[1].checked == true ){
-	}else{
+	
+	
+	if (form.sinoGarganta[0].checked == true) {
+		sintomas++;
+	}
+	else if (form.sinoGarganta[1].checked == true) {
+	}
+	else{
 		alert("Responde si tienes dolor de garganta");
 		return false;
 	}
-
-	if (form.sinoRespirar[0].checked == true || form.sinoRespirar[1].checked == true ){
-	}else{
+	
+	
+	if (form.sinoRespirar[0].checked == true) {
+		sintomas++;
+	}
+	else if (form.sinoRespirar[1].checked == true) {
+	}
+	else{
 		alert("Responde si tienes dificultad para respirar");
 		return false;
 	}
-
-
-	//Si todos los campos han validado correctamente, se envía el formulario
-	document.form.submit();
+	
+	//Si todos los campos han validado correctamente, al presionar "Enviar" salta un alert indicando los sintomas marcados y se envía el formulario
+	
+	alert("El formulario fue completado correctamente. " + sintomas + " síntomas de COVID-19 fueron registrados.");
+	document.form.submit(); 
 }
+
+
